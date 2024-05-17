9f760de663310bee9d10b82e60eddc286bd747c3
ffouquet42 <ffouquet42@gmail.com>
2024-05-17 19:59:46 +0200

    discover lua and try to update existing mods

---
+++ mods/smokers/media/lua/client/smokers.lua
@@ -0,0 +1,20 @@
+-- local function OnNewGame(_player)
+
+-- 	local player = getSpecificPlayer(0)
+
+-- 	if player:HasTrait("Smoker") then
+-- 		player:getInventory():AddItem("Base.Cigarettes");	
+-- 	end
+-- end
+
+-- local function OnCreateSurvivor(survivor)
+
+-- 	local player = getSpecificPlayer(0)
+
+-- 	if player:HasTrait("Smoker") then
+-- 		player:getInventory():AddItem("Base.Cigarettes");	
+-- 	end
+-- end
+
+-- Events.OnNewGame.Add(OnNewGame)
+-- Events.OnCreateSurvivor.Add(OnCreateSurvivor)
