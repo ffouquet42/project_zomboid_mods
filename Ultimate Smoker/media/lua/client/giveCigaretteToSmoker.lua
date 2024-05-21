6148858649de61af2e86304cc8c85591a8a87629
ffouquet42 <ffouquet42@gmail.com>
2024-05-21 04:27:46 +0200

    can start with empty or refillable lighter

--- Ultimate Smoker/media/lua/client/giveCigaretteToSmoker.lua
+++ Ultimate Smoker/media/lua/client/giveCigaretteToSmoker.lua
@@ -1,5 +1,6 @@
 --[[ 
 Ultimate Smoker 41.78.16 stable
+This mod may be not compatible with other smokers mods
 SP : OK
 MP : TD
 Github: ffouquet42
@@ -19,15 +20,20 @@ local function giveCigarettesToSmoker(playerIndex, player)
         end
 		
 
-		-- Provide a way to light a cigarette (70% - Lighter / 25% - Matches / 5% - Nothing) with more or less use remaining
+		-- Provide a way to light a cigarette with more or less use remaining
 		local lighterOrMatch = ZombRand(1, 101)
 		
-		if lighterOrMatch <= 70 then
+		if lighterOrMatch <= 50 then
 			local lighterUsed = player:getInventory():AddItem("Base.Lighter")
 			lighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
-		elseif lighterOrMatch <= 95 then
+		elseif lighterOrMatch <= 65 then
 			local MatchesUsed = player:getInventory():AddItem("Base.Matches")
 			MatchesUsed:setUsedDelta(ZombRand(1, 11) / 10)
+		elseif lighterOrMatch <= 85 then
+			local RefillableLighterUsed = player:getInventory():AddItem("Base.RefillableLighter")
+			RefillableLighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
+		elseif lighterOrMatch <= 95 then
+			local EmptyLighter = player:getInventory():AddItem("Base.EmptyLighter")
 		else -- useless?
 		end
 		
@@ -37,7 +43,7 @@ end
 
 -- local function onKeyPressed(key)
 -- 	local player = getPlayer()
---     if key == 16 then -- 81?
+--     if key == 16 then
 -- 		giveCigarettesToSmoker(player:getID(), player)
 --     end
 -- end
