6e570a742dc17d7b52c1f8d16a1a938965daf0db
ffouquet42 <ffouquet42@gmail.com>
2024-05-30 17:40:43 +0200

    start distributions rate

---
+++ refillable_blue_lighter/Contents/mods/refillable_blue_lighter/media/lua/server/Items/RBL_Distributions.lua
@@ -0,0 +1,16 @@
+require 'Items/ProceduralDistributions'
+
+-- table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "Base.RefillableLighter");
+-- table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "Base.RefillableLighter");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, "Base.RefillableLighter");
+table.insert(ProceduralDistributions["list"]["LivingRoomShelf"].items, 0.7);
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, "Base.RefillableLighter");
+table.insert(ProceduralDistributions["list"]["CrateBooks"].items, 1);
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, "Base.RefillableLighter");
+table.insert(ProceduralDistributions["list"]["GunStoreMagazineRack"].items, 1);
+
+-- table.insert(ProceduralDistributions["list"]["allInventoryFemale"].items, "Base.RefillableLighter");
+-- table.insert(ProceduralDistributions["list"]["allInventoryFemale"].items, 0.5);
