57ae9df79bb3eb2408c0e77b228041f18f30c29c
ffouquet42 <ffouquet42@gmail.com>
2024-05-30 17:40:14 +0200

    fix bug that give items on each connection

--- smoker_start_with_cigarettes/Contents/mods/smoker_start_with_cigarettes/media/lua/client/giveCigaretteToSmoker.lua
+++ smoker_start_with_cigarettes/Contents/mods/smoker_start_with_cigarettes/media/lua/client/giveCigaretteToSmoker.lua
@@ -1,5 +1,5 @@
---[[ 
-Ultimate Smoker 41.78.16 stable
+--[[
+Smoker start with cigarettes 41.78.16 stable
 This mod may be not compatible with other smokers mods
 SP : OK
 MP : TD
@@ -8,33 +8,40 @@ Github: ffouquet42
 
 local function giveCigarettesToSmoker(playerIndex, player)
 
-	-- Ensure character have Smoker trait
+    -- Ensure character has Smoker trait
     if player:HasTrait("Smoker") then
 
-		-- Give between 0 and 20 cigarettes
-        local cigarettesToGive = ZombRand(21)
+        -- Check if the items have already been given
+        if not player:getModData().cigarettesGiven then
 
-        for i = 1, cigarettesToGive do
-            local cigarette = InventoryItemFactory.CreateItem("Base.Cigarettes")
-            player:getInventory():AddItem(cigarette)
+            -- Give between 0 and 20 cigarettes
+            local cigarettesToGive = ZombRand(21)
+
+            for i = 1, cigarettesToGive do
+                local cigarette = InventoryItemFactory.CreateItem("Base.Cigarettes")
+                player:getInventory():AddItem(cigarette)
+            end
+            
+            -- Provide a way to light a cigarette with more or less use remaining
+            local lighterOrMatch = ZombRand(1, 101)
+            
+            if lighterOrMatch <= 50 then -- 50% Base Lighter
+                local lighterUsed = player:getInventory():AddItem("Base.Lighter")
+                lighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
+            elseif lighterOrMatch <= 65 then -- 15% Base Matches
+                local MatchesUsed = player:getInventory():AddItem("Base.Matches")
+                MatchesUsed:setUsedDelta(ZombRand(1, 11) / 10)
+            elseif lighterOrMatch <= 85 then -- 20% Refillable Lighter
+                local RefillableLighterUsed = player:getInventory():AddItem("Base.RefillableLighter")
+                RefillableLighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
+            elseif lighterOrMatch <= 95 then -- 10% Empty Lighter
+                local EmptyLighter = player:getInventory():AddItem("Base.EmptyLighter")
+            else -- 5% Nothing
+            end
+
+            -- Mark that the items have been given
+            player:getModData().cigarettesGiven = true
         end
-		
-		-- Provide a way to light a cigarette with more or less use remaining
-		local lighterOrMatch = ZombRand(1, 101)
-		
-		if lighterOrMatch <= 50 then -- 50% Base Lighter
-			local lighterUsed = player:getInventory():AddItem("Base.Lighter")
-			lighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
-		elseif lighterOrMatch <= 65 then -- 15% Base Matches
-			local MatchesUsed = player:getInventory():AddItem("Base.Matches")
-			MatchesUsed:setUsedDelta(ZombRand(1, 11) / 10)
-		elseif lighterOrMatch <= 85 then -- 20% Refillable Lighter
-			local RefillableLighterUsed = player:getInventory():AddItem("Base.RefillableLighter")
-			RefillableLighterUsed:setUsedDelta(ZombRand(1, 11) / 10)
-		elseif lighterOrMatch <= 95 then -- 10% Empty Lighter
-			local EmptyLighter = player:getInventory():AddItem("Base.EmptyLighter")
-		else -- 5% Nothing
-		end
     end
 end
 
